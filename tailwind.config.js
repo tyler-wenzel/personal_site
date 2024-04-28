35f19fef85fd2ff253381a7c7cf6dda8b3f22ae3
Tyler Wenzel <tyler.gh3@gmail.com>
2024-04-27 20:53:39 -0400

    commit v1 of portfolio site

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ["./index.html", "./**/*.html", "./**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        loafgold: "#FDE68A",
+        burnttoast: "#292524",
+        black: "#000000",
+        grey: "#4D4D4D",
+        link: {
+          normal: "#4c74b9",
+          decoration: "#d2dcee",
+          hover: "#829dce",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
